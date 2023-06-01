28512eddf053acb16aa62e2e9611f68ebc281f38
Noraa Stoke <noraa.july.stoke@gmail.com>
2023-06-01 08:25:04 -0400

    ok

--- noraas_notes/src/main.rs
+++ noraas_notes/src/main.rs
@@ -3,11 +3,13 @@ mod control_flow;
 mod rust_basics;
 mod drink_stand;
 mod vectors;
+mod webview_3;
 
 fn main() {
     // math_and_logic_operators::main();
     // control_flow::main();
     // rust_basics::main();
     // drink_stand::main();
-    vectors::main();
+    // vectors::main();
+    webview_3::main();
 }
